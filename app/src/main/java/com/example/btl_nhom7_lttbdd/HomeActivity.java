5b5c9b4cc4dd1cb3f9a43750dca99f236117d375
Plun <phuchoangxyz@gmail.com>
2022-05-16 02:59:32 +0700

    Phuc_them Navigation

--- app/src/main/java/com/example/btl_nhom7_lttbdd/HomeActivity.java
+++ app/src/main/java/com/example/btl_nhom7_lttbdd/HomeActivity.java
@@ -3,47 +3,66 @@ package com.example.btl_nhom7_lttbdd;
 import android.annotation.SuppressLint;
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
-
+    private Uri imageURI;
+    private StorageReference storageReference;
     private Toolbar toolbar;
     private RecyclerView recyclerView;
     private FloatingActionButton floatingActionButton;
+    private DrawerLayout mDrawerLayout;
+    private ImageView imgAvatar;
+    private TextView tvName, tvEmail;
 
+    private NavigationView mNavigationView;
     private DatabaseReference reference;
     private FirebaseAuth mAuth;
     private FirebaseUser mUser;
@@ -54,18 +73,26 @@ public class HomeActivity extends AppCompatActivity {
     private String key = "";
     private String task;
     private String description;
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_home);
 
-        toolbar = findViewById(R.id.homeToolbar);
+        toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("Todo List App");
         mAuth = FirebaseAuth.getInstance();
+
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, mDrawerLayout, toolbar,
+                R.string.nav_drawer_open, R.string.nav_drawer_close);
+        mDrawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        initUI();
+        mNavigationView.setNavigationItemSelectedListener(this);
+
+        mNavigationView.getMenu().findItem(R.id.nav_home).setChecked(true);
 
         recyclerView = findViewById(R.id.recyclerView);
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
@@ -75,11 +102,11 @@ public class HomeActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(linearLayoutManager);
 
         loader = new ProgressDialog(this);
-
-
+        showInfor();
         mUser = mAuth.getCurrentUser();
         onlineUserID = mUser.getUid();
         reference = FirebaseDatabase.getInstance().getReference().child("tasks").child(onlineUserID);
+
 
         floatingActionButton = findViewById(R.id.fab);
         floatingActionButton.setOnClickListener(new View.OnClickListener() {
@@ -88,6 +115,30 @@ public class HomeActivity extends AppCompatActivity {
                 addTask();
             }
         });
+    }
+
+    private void initUI(){
+        mNavigationView = findViewById(R.id.navigation_view);
+        imgAvatar = mNavigationView.getHeaderView(0).findViewById(R.id.img_ava);
+        tvName = mNavigationView.getHeaderView(0).findViewById(R.id.tv_ten);
+        tvEmail = mNavigationView.getHeaderView(0).findViewById(R.id.tv_mail);
+    }
+    private void showInfor(){
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if(user == null){
+            return;
+        }
+        String name = user.getDisplayName();
+        String email = user.getEmail();
+        Uri photoUrl = user.getPhotoUrl();
+        if(name == null){
+            tvName.setVisibility(View.GONE);
+        }else {
+            tvName.setVisibility(View.VISIBLE);
+            tvName.setText(name);
+        }
+        tvEmail.setText(email);
+        Glide.with(this).load(photoUrl).error(R.drawable.ic_launcher_round).into(imgAvatar);
     }
 
     private void addTask() {
@@ -197,7 +248,77 @@ public class HomeActivity extends AppCompatActivity {
         adapter.startListening();
     }
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.nav_home){
+            Intent intent = new Intent(this, HomeActivity.class);
+            mNavigationView.getMenu().findItem(R.id.nav_home).setChecked(true);
+            startActivity(intent);
+        }else if(id == R.id.nav_profile){
+            updateProfile();
+            mNavigationView.getMenu().findItem(R.id.nav_profile).setChecked(true);
+        }else if(id == R.id.nav_change){
 
+            mNavigationView.getMenu().findItem(R.id.nav_change).setChecked(true);
+        }else if(id == R.id.log_out){
+            mAuth.signOut();
+            Intent intent  = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+        }
+        mDrawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    private void updateProfile() {
+        AlertDialog.Builder myDialog = new AlertDialog.Builder(this);
+        LayoutInflater inflater = LayoutInflater.from(this);
+
+        View myView = inflater.inflate(R.layout.my_profile, null);
+        myDialog.setView(myView);
+
+        final AlertDialog dialog = myDialog.create();
+        dialog.setCancelable(false);
+
+        final EditText edtFullname = myView.findViewById(R.id.edt_fullname);
+        final EditText edtEmail = myView.findViewById(R.id.edt_email);
+        final ImageView imageAvatar = myView.findViewById(R.id.img_avatar_update);
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if(user == null){
+            return;
+        }
+        edtFullname.setText(user.getDisplayName());
+        edtEmail.setText(user.getEmail());
+        imageAvatar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                selectImage();
+            }
+        });
+
+        Button save = myView.findViewById(R.id.btn_update_profile);
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loader.setMessage("Adding your data");
+                loader.setCanceledOnTouchOutside(false);
+                loader.show();
+                dialog.dismiss();
+            }
+        });
+
+        dialog.show();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(mDrawerLayout.isDrawerOpen(GravityCompat.START)){
+            mDrawerLayout.closeDrawer(GravityCompat.START);
+        }else{
+            super.onBackPressed();
+        }
+    }
 
     public static class MyViewHolder extends RecyclerView.ViewHolder {
         View mView;
@@ -290,25 +411,52 @@ public class HomeActivity extends AppCompatActivity {
 
         dialog.show();
     }
+    private void uploadImage() {
+        loader = new ProgressDialog(this);
+        loader.setTitle("Uploading File...");
+        loader.show();
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main_menu, menu);
-        return super.onCreateOptionsMenu(menu);
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy_MM_dd_HH_mm_ss", Locale.ENGLISH);
+        Date now = new Date();
+        String fileName = simpleDateFormat.format(now);
+        storageReference = FirebaseStorage.getInstance().getReference("image/"+fileName);
+
+        storageReference.putFile(imageURI).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                ImageView img = findViewById(R.id.img_avatar_update);
+                img.setImageURI(null);
+                Toast.makeText(HomeActivity.this, "Successfully Upload", Toast.LENGTH_SHORT).show();
+                if(loader.isShowing()){
+                    loader.dismiss();
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                if(loader.isShowing()){
+                    loader.dismiss();
+                }
+                Toast.makeText(HomeActivity.this, "Failed to Upload", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void selectImage() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, 100);
     }
 
     @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()){
-            case R.id.logout:
-                mAuth.signOut();
-                Intent intent  = new Intent(HomeActivity.this, LoginActivity.class);
-                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                startActivity(intent);
-                finish();
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == 100 && resultCode == RESULT_OK && data != null){
+            imageURI = data.getData();
+            ImageView imageAvatar = (ImageView)findViewById(R.id.img_avatar_update);
+            imageAvatar.setImageURI(imageURI);
         }
-        return super.onOptionsItemSelected(item);
     }
-
-
 }
